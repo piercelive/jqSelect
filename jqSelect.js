09b2f273c6c619ee827d816b6b22b7aedb2ff5a8
Grant <gdpierce90@gmail.com>
2013-05-20 15:53:27 -0500

    basic proof of concept grabbing the values of the selector

--- jqSelect.js
+++ jqSelect.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-	alert('jqSelect');
-});
+var jqSelect = function(opts){
+	var options = {
+		selector : 'select',
+		slide : true,
+		dropdownClass : 'dropdown',
+		arrowClass : 'arrow',
+		optionClass : 'option'
+	}
+	$.extend(options, opts);
+
+	var selectBoxes = $(options.selector);
+	$.each(selectBoxes, function() {
+		var optionElements = $(this).children('option');
+		$.each(optionElements, function(){
+			var $el = $(this);
+			$('body').append('<li>'+$el.val()+'</li>');
+		});
+	});	
+}
+
+
