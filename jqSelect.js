fbe8de3b521ddf8f63a49f60937744f1069712cd
Grant <gdpierce90@gmail.com>
2013-05-21 16:21:15 -0500

    triggering the change events

--- jqSelect.js
+++ jqSelect.js
@@ -4,18 +4,44 @@ var jqSelect = function(opts){
 		slide : true,
 		dropdownClass : 'dropdown',
 		arrowClass : 'arrow',
-		optionClass : 'option'
+		optionClass : 'option',
+		fakeBoxClass : 'fake'
 	}
 	$.extend(options, opts);
 
 	var selectBoxes = $(options.selector);
 	$.each(selectBoxes, function() {
-		var optionElements = $(this).children('option');
-		$.each(optionElements, function(){
-			var $el = $(this);
-			$('body').append('<li>'+$el.val()+'</li>');
+		var $selectBox = $(this);
+		var $fakeBox = $('<h1/>', {
+			"class" : options.fakeBoxClass,
+			text: $selectBox.find(":selected").text(),
+			"data-value": $selectBox.val()	
 		});
+		var $container = $selectBox.parent();
+		var optionElements = $selectBox.children('option');
+		var newOptions = [];
+		$.each(optionElements, function(){
+			var $option = $(this);
+			var val = $option.val();
+			var text = $option.text();
+			var $newOption = $('<span/>',{
+				"class" : options.optionClass,
+				"data-value" : val,
+				text : text						
+			});
+			newOptions.push($newOption);
+		});
+		$container.append($fakeBox);
+		$.each(newOptions, function(){
+			var $that = $(this);
+			$that.on('click', function(){
+				$selectBox.trigger('change');
+				$fakeBox.attr('data-value', $that.data('value'));
+				$fakeBox.text($that.text());
+			});
+			$container.append($that);
+		});
+		
 	});	
 }
-
 
