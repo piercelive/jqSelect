510cce4ec192e931952b4ba13cfde37cb24eadd1
Grant <gdpierce90@gmail.com>
2013-05-21 21:08:22 -0500

    converted to jquery plugin format, and refactored a bit

--- jqSelect.js
+++ jqSelect.js
@@ -1,78 +1,130 @@
-var jqSelect = function(opts){
-	var options = {
-		selector : 'select',
-		slide : true,
-		dropdownClass : 'dropdown',
-		arrowClass : 'arrow',
-		optionClass : 'option',
-		fakeBoxClass : 'fake',
-		debug : false
-	}
-	$.extend(options, opts);
-	debugMode = options.debug;
+(function($){
 
-	var selectBoxes = $(options.selector);
-	$.each(selectBoxes, function() {
-		var $selectBox = $(this);
-		if(!options.debug){
-			$selectBox.hide();
+	var JQselect = function(el, options) {
+
+		//Defaults:
+		this.defaults = {		
+			slide : true,
+			dropdownClass : 'dropdown',
+			optionClass : 'option',
+			fakeBoxClass : 'fake',
+			debugMode : false
+		};
+		//elements:
+		this.$fakeBox;
+		this.$dropdown;
+		this.$surroundingDiv;
+		this.$container;
+
+		//Extending options:
+		this.opts = $.extend({}, this.defaults, options);
+
+		//Privates:
+		this.$el = $(el);
+		this.$optionList = this.$el.find('option');
+	}
+
+	// Separate functionality from object creation
+	JQselect.prototype = {
+
+		init: function() {
+			var _this = this;
+			var $el = _this.$el;
+
+			//set up the elements we are using
+			_this.$container = $el.parent();
+			_this.$fakeBox = _this.createFakeBox();
+			_this.$dropdown = _this.createDropDown();
+			_this.$surroundingDiv = _this.createSurroundingDiv();
+			
+			//set up the click events
+			_this.setupClicks();
+			
+			
+			//hide the original select box
+			if(!_this.opts.debugMode){
+				$el.hide();
+			}
+			
+			//add elements to the dom
+			_this.$surroundingDiv.append(_this.$fakeBox);
+			_this.$surroundingDiv.append(_this.$dropdown);
+			_this.$container.append(_this.$surroundingDiv);
+			
+						
+		},
+
+		
+		debug: function(message) {
+			var _this = this;
+			if(_this.debugMode){
+				console.log(message);
+			}
+		},
+		createFakeBox: function(){
+			var _this = this;
+			return $('<span/>', {
+				"class" : _this.opts.fakeBoxClass,
+				text: _this.$el.find(":selected").text(),
+				"data-value": _this.$el.val()	
+			});
+		},
+		createDropDown: function(){
+
+			var _this = this;
+			$dropdown = $('<div/>', {
+				"class": _this.opts.dropdownClass
+			});
+			$.each(_this.$optionList, function(){
+				var $that = $(this);
+
+				var $newOption = $('<span/>',{
+					"class" : _this.opts.optionClass,
+					"data-value" : $that.val(),
+					text : $that.text()						
+				});
+
+				$newOption.on('click', function(){
+					_this.$el.trigger('change');
+					_this.$fakeBox.attr('data-value', $that.val());
+					_this.$fakeBox.text($that.text());
+					_this.$el.val($that.val());
+				});
+				$dropdown.append($newOption);
+			});
+			return $dropdown;
+		},
+		setupClicks: function(){
+			var _this = this;
+			$('html').on('click', function(){
+				$dropdown.hide();
+			});
+			_this.$fakeBox.on('click', function(e){
+				e.stopPropagation();
+				if(_this.slide){
+					$dropdown.slideToggle();
+				}else{
+					$dropdown.toggle();	
+				}
+			});
+		},
+		createSurroundingDiv: function() {
+			
+			return $('<div/>', {
+				"class" : "jq-select"
+			});
 		}
-		var $fakeBox = $('<span/>', {
-			"class" : options.fakeBoxClass,
-			text: $selectBox.find(":selected").text(),
-			"data-value": $selectBox.val()	
-		});
-		var $container = $selectBox.parent();
-		var optionElements = $selectBox.children('option');
-		var newOptions = [];
-		$.each(optionElements, function(){
-			var $option = $(this);
-			var val = $option.val();
-			var text = $option.text();
-			var $newOption = $('<span/>',{
-				"class" : options.optionClass,
-				"data-value" : val,
-				text : text						
-			});
-			newOptions.push($newOption);
-		});
-		$dropdown = $('<div/>', {
-			"class": options.dropdownClass
-		});
-		$.each(newOptions, function(){
-			var $that = $(this);
-			$that.on('click', function(){
-				$selectBox.trigger('change');
-				$fakeBox.attr('data-value', $that.data('value'));
-				$fakeBox.text($that.text());
-				$selectBox.val($that.data('value'));
-			});
-			$dropdown.append($that);
-		});
-		$arrow = $('<span/>', {
-			"class" : options.arrowClass
-		});
-		$arrow.on('click', function(e){
-			e.stopPropagation();
-			$dropdown.show();
-		});
-		$surroundingDiv = $('<div/>', {
-			"class" : "jq-select"
-		});
-		$surroundingDiv.append($fakeBox);
-		$surroundingDiv.append($arrow);
-		$surroundingDiv.append($dropdown);
-		$container.append($surroundingDiv);
-		$('body').on('click', function(){
-			$dropdown.hide();
-		});
-		
-		
-	});	
-}
-var debug = function(message) {
-	if(debugMode){
-		console.log(message);
-	}
-}
 
+	};
+
+	// The actual plugin
+	$.fn.jqSelect = function(options) {
+		if(this.length) {
+			this.each(function() {
+				var rev = new JQselect(this, options);
+				rev.init();
+				$(this).data('jq-select', rev);
+			});
+		}
+	};
+})(jQuery);
